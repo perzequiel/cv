eea621222cc4181e41247622475caf0e310ab7cb
Perzequiel <ezequielgonzalezt@gmail.com>
2025-10-23 23:22:25 -0300

    first commit

---
+++ PERZE.md
@@ -0,0 +1,4 @@
+https://www.overleaf.com/project/68eda76291b863897e1bd2f7
+
+
+./build.sh
---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Limpiar archivos auxiliares
+/Library/TeX/texbin/latexmk -C
+
+# Compilar PDF
+/Library/TeX/texbin/latexmk -pdf template.tex
